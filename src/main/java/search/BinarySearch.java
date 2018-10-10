5b168fda13658c183a01b3d0d0a7167a2efcf2f0
Victor Florintsev <victorflorintsev1996@gmail.com>
2018-10-10 01:15:21 -0500

    created BinarySearch skeleton and renames, etc

---
+++ src/main/java/search/BinarySearch.java
@@ -0,0 +1,15 @@
+package java.search;
+
+import java.util.List;
+
+public class BinarySearch {
+    /**
+     * Returns the index at which the element exists in a sorted List. Returns -1 if not found.
+     * @param element the Comparable to search for
+     * @param list the pre-sorted List
+     * @return
+     */
+    public int inSortedList(Comparable element, List<Comparable> list) {
+        return -1;
+    }
+}
--- src/main/java/sort/Sorters.java
+++ src/main/java/sort/Sorters.java
@@ -1,4 +1,4 @@
-package java.sorters;
+package java.sort;
 
 import java.util.List;
 
