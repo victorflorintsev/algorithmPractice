d92ccafc16d29231d45fa4a49b1c26495516ed2d
Victor Florintsev <victorflorintsev1996@gmail.com>
2018-10-10 13:49:57 -0500

    added skeleton for inserting into an array

---
+++ src/main/java/VariousAlgorithmsHelper.java
@@ -0,0 +1,11 @@
+public class VariousAlgorithmsHelper {
+    /**
+     * Inserts an element at the correct position into an array efficiently, resizing if need be.
+     * @param element
+     * @param array
+     * @param <T>
+     */
+    public <T extends Comparable<? super T>> void insertIntoSortedArray(T element, T[] array) {
+
+    }
+}
